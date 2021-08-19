bee45e08dc5fbad76d0e8643d05aa24efb9a908d
Халтобин Евгений <evgeny.khaltobine@gmail.com>
2021-08-19 14:18:30 +0300

    added micord patches

---
+++ apply-micord-patches.bat
@@ -0,0 +1,4 @@
+git apply --reject --ignore-whitespace micord.patch
+git apply --reject --ignore-whitespace micord-SUPPORT-5401.patch
+rem git apply --reject --ignore-whitespace micord-WEBBPMNEXT-2544-task-due-date.patch
+rem git apply --reject --ignore-whitespace micord-WEBBPMNEXT-2554-message-correlation.patch
